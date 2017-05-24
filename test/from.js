8467c66e02a5259be23a94bb413dd963769f16c9
Lorenzo Ruiz <lars.bs@hotmail.com>
2017-05-24 14:32:42 +0200

    Add tests to fromValue

--- test/from.js
+++ test/from.js
@@ -1,22 +1,22 @@
 import { expect } from 'chai';
-import from from '../src/from';
+import fromKey from '../src/from';
 
 
-describe('FROM', function () {
-  describe('from(fromKey)', function () {
+describe('from', function () {
+  describe('fromKey(fromKey)', function () {
     it('should return an object with `fromKey` as a prop', function () {
-      const result = from('test');
+      const result = fromKey('test');
       expect(result.fromKey).to.equal('test');
     });
     it('should return an object with `transform` as a prop', function () {
-      const result = from('test');
+      const result = fromKey('test');
       expect(result.transform).to.exist;
     });
     it('should allow concatenation of transformations', function () {
-      expect(() => from('test').defaultTo('test').toString()).to.not.throw(Error);
+      expect(() => fromKey('test').defaultTo('test').toString()).to.not.throw(Error);
     });
     it('should apply transform functions in the order they were concatenated', function () {
-      const result = from('test').defaultTo(false).toString();
+      const result = fromKey('test').defaultTo(false).toString();
       expect(result.transform(undefined)).to.equal('false');
     });
   });
